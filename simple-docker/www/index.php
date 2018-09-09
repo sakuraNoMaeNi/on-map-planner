8a67b4bb9031ed9a672156afea69c805cef2fab8
sakuraNoMaeNi <sven@sitework.be>
2018-09-09 16:59:40 +0200

    basic homepage
    Signed-off-by: sakuraNoMaeNi <sven@sitework.be>

---
+++ simple-docker/www/index.php
@@ -0,0 +1,36 @@
+<?php
+namespace omp;
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../private/init.php';
+?>
+
+<!doctype html>
+<html lang="nl">
+<head>
+	<meta charset="utf-8">
+	<title>OMP</title>
+	<meta name="description" content="On map planner">
+	<meta name="author" content="sakuraNoMaeNi & ">
+	<link rel="stylesheet" href="styles/base.css">
+</head>
+<body>
+	<script src="scripts/base.js"></script>
+	<header>
+		<a>Logo fietsersbond</a>
+		<nav>
+			<ul>
+				<li><a>Afdeling Destelbergen</a></li>
+				<li>filters</li>
+				<li>andere pagina 1</li>
+				<li>andere pagina 2</li>
+			</ul>
+		</nav>
+		<a>aanmelden</a>
+	</header>
+	<aside>
+		intro & details van geselecteerde elementen
+	</aside>
+	<section>
+		de kaart komt hier
+	</section>
+</body>
+</html>
